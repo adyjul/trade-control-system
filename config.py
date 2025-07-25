208b20be117ce2d3fbe28827375af0ace79fe5d0
Ady Juliyanto <adyjuliyanto2001@gmail.com>
2025-07-26 00:29:25 +0700

    fix some error

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ BINANCE_TESTNET = os.getenv("BINANCE_TESTNET", "false").lower() == "true"
 LEVERAGE_DEFAULT = int(os.getenv("LEVERAGE_DEFAULT", 10))
 POSITION_SIZE_USDT = float(os.getenv("POSITION_SIZE_USDT", 50))
 DB_PATH = os.getenv("DB_PATH", "db/bot.db")
-PREDICT_DIR = os.getenv("PREDICT_DIR", "./predict_output")
+PREDICT_DIR = os.getenv("PREDICT_DIR", "./https://github.com/adyjul/trade-control-system.git")
 DATA_DIR = os.getenv("DATA_DIR", "./data")
 TIMEZONE_OFFSET_HOURS = int(os.getenv("TIMEZONE_OFFSET_HOURS", 7))
 PAPER_TRADING = os.getenv("PAPER_TRADING", "true").lower() == "true"
