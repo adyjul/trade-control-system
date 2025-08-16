5b9e11f72690d1827d8ade752929ff7fb54af335
Ady Juliyanto <adyjuliyanto2001@gmail.com>
2025-08-16 22:31:25 +0700

    fix predictor

--- strategy/backtest.py
+++ strategy/backtest.py
@@ -324,10 +324,10 @@ def run_full_backtest(
         # --- sinyal ---
         df['signal'] = None  
         if df.iloc[-1]["sideways_signal"]:
-            df['signal'] = "HOLD_SIDEWAY"
+            df['signal'] = "HOLD"
         else:
             df['signal'] = df.apply(detect_signal, axis=1)
-            
+
         df = apply_filters(df)
         df['is_fake_breakout'] = df.apply(detect_breakout, axis=1)
         df = detect_potential_breakout(df)
