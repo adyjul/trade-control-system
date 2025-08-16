f92cd7f6da2fad7fa95e4f74436997f6d8546245
Ady Juliyanto <adyjuliyanto2001@gmail.com>
2025-08-16 22:49:26 +0700

    fix predictor

--- strategy/backtest.py
+++ strategy/backtest.py
@@ -331,7 +331,7 @@ def run_full_backtest(
         #     df = apply_filters(df)
 
         df['signal'] = df.apply(detect_signal, axis=1)
-        df = apply_filters(df)
+        # df = apply_filters(df)
         df['is_fake_breakout'] = df.apply(detect_breakout, axis=1)
         df = detect_potential_breakout(df)
 
