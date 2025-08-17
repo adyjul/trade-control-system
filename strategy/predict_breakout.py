87c39c81428702f2ffb1f0b69301944cd5ead5c2
Ady Juliyanto <adyjuliyanto2001@gmail.com>
2025-08-17 19:31:42 +0700

    add train model with lookback

--- strategy/predict_breakout.py
+++ strategy/predict_breakout.py
@@ -7,7 +7,7 @@ import sys
 import numpy as np
 
 # ------- CONFIG -------
-MODEL_PATH = "/root/trade-control-system/strategy/ml/models/breakout_rf_model_avaxusdt_1h.pkl"
+MODEL_PATH = "/root/trade-control-system/strategy/ml/models/breakout_rf_model_avaxusdt.pkl"
 BACKTEST_GLOB = "/root/trade-control-system/backtest_result/hasil_backtest_avaxusdt_1h.xlsx"  # sesuaikan timeframe/pair jika perlu
 # BACKTEST_GLOB = "/root/trade-control-system/data_predict/AVAXUSDT_1h_full.xlsx"
 OUTPUT_PRED_PATH = "/root/trade-control-system/backtest_result/predicted_result_single_pair.xlsx"
