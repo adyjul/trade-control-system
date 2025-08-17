9584cb1a20ad7ec63d5accf5b1cbc4d5754bd8bb
Ady Juliyanto <adyjuliyanto2001@gmail.com>
2025-08-17 18:52:53 +0700

    add false_reversal

--- strategy/ml/utils.py
+++ strategy/ml/utils.py
@@ -27,6 +27,7 @@ def predict_ml_signal(model, row: pd.Series) -> bool:
         'upper_band', 'lower_band', 'bb_percentile',
         'support', 'resistance', 'atr_multiple',
         'is_potential_breakout', 'entry_signal',
+        'false_reversal',
         'macd', 'macd_signal', 'macd_hist', 'signal_numeric'
         
     ]
