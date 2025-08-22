49f6721217bed5e5ed2fd1dddbb80c00fbc879e9
Ady Juliyanto <adyjuliyanto2001@gmail.com>
2025-08-22 11:45:51 +0700

    add menu

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ def new_bot_data():
 
     status = request.args.get("status")
     err = request.args.get("err")
-    return render_template("backtest_form.html",status=status,err=err)
+    return render_template("backtest_form_data.html",status=status,err=err)
 
 @app.route("/make_model", methods=["GET"])
 def make_model():
