9a057fc577dc7f403001b08a659cb03d723c509d
aadfahmi <aadfahmi@gmail.com>
2025-09-24 10:51:45 +0700

    fix some error'

--- strategy/coba_coba.py
+++ strategy/coba_coba.py
@@ -20,5 +20,5 @@ entry = 34.209
 
 
 # Long contoh
-print(calc_profit_percent(entry, "LONG", 34.104))   # +0.05 (5%)
-print(calc_profit_percent(entry, "LONG",  34.324))   # -0.05 (-5%)
+print(calc_profit_percent(entry, "SHORT", 34.104))   # +0.05 (5%)
+print(calc_profit_percent(entry, "SHORT",  34.324))   # -0.05 (-5%)
