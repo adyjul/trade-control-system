7ebb0f05ef2c32b11cb9a1927787c8209f091323
aadfahmi <aadfahmi@gmail.com>
2025-09-30 22:46:48 +0700

    add emergency tp

--- strategy/live_trade_bot6.py
+++ strategy/live_trade_bot6.py
@@ -452,6 +452,7 @@ class ImprovedLiveDualEntryBot:
         """
         Membatalkan limit order yang arah-nya sudah tidak sama dengan sinyal aktif terakhir.
         """
+        print(self._current_signal_side)
         # print('masuk cancel misaligned orders')
         if self._current_signal_side is None:
             return
