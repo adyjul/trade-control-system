c1b75a6b0ceaa17491c18e26ac5a5ce090c86c06
adyjuliyanto2001 <adyjuliyanto2001@gmail.com>
2025-12-14 22:29:51 +0700

    check bug

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ BINANCE_API_SECRET = os.getenv("BINANCE_API_SECRET")
 BINANCE_TESTNET = os.getenv("BINANCE_TESTNET", "false").lower() == "true"
 LEVERAGE_DEFAULT = int(os.getenv("LEVERAGE_DEFAULT", 10))
 POSITION_SIZE_USDT = float(os.getenv("POSITION_SIZE_USDT", 50))
-DB_PATH = os.getenv("DB_PATH", "/trade-control-system/db/bot.db")
+DB_PATH = os.getenv("DB_PATH", "/home/julbot/trade-control-system/db/bot.db")
 PREDICT_DIR = os.getenv("PREDICT_DIR", "./predict_output")
 DATA_DIR = os.getenv("DATA_DIR", "./data")
 TIMEZONE_OFFSET_HOURS = int(os.getenv("TIMEZONE_OFFSET_HOURS", 7))
