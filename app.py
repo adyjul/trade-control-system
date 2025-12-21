50b34abdf509069ab897532385d7b88c463a0950
adyjuliyanto2001 <adyjuliyanto2001@gmail.com>
2025-12-21 23:04:39 +0700

    fix

--- app.py
+++ app.py
@@ -183,5 +183,7 @@ def backtest_summary_data():
     return render_template("summary_backtest.html", summary=summary)
 
 
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=8000, debug=True)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=False)
